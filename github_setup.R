de3c9449b7615c2c0d48697be011ec7c9c197e86
VinceThu <vincent.thuillier2@sanofi.com>
2024-05-29 09:04:28 +0200

    initial commit of github_setup.R

---
+++ github_setup.R
@@ -0,0 +1,36 @@
+#/*****************************************************************************
+# Property of SANOFI
+#
+# Program name     : github_setup.R  
+# 
+# Description      : training with https://happygitwithr.com  
+#   
+# Author           : V. THUILLIER (fr59847)
+# 
+# Compound number  : SARXXXXXX  
+# Study code       : EFCXXXXXX  
+# Analysis code    : CSR  
+#   
+# Date completed   : 29 mai 2024  
+#   
+# Input files      : files w paths 
+#   
+# Input programs   : pgms w paths   
+#   
+# Packages needed  : pack w paths
+#   
+# Outputs created  : outputs w paths  
+#   
+# R version        : R version 4.3.2 (2023-10-31 ucrt)  
+# Platform         : Windows 10 x64 - EMEA4TSJ5YSRPJP  
+#******************************************************************************/    
+
+#/*****************************************************************************
+# _ https://happygitwithr.com/new-github-first  -----
+#******************************************************************************/  
+
+usethis::create_from_github(
+  "https://github.com/VinceThu/git_r_ch20",
+  destdir = "C:/Users/fr59847/OneDrive - Sanofi/Documents"
+)
+  
